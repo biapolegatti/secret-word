b0cfbd4c9a369af0b344af35186fd7e2d35700a4
biapolegatti <bianca.polegatti@gmail.com>
2023-05-28 20:26:50 -0300

    first commit

---
+++ src/components/StartScreen.js
@@ -0,0 +1,14 @@
+import "./StartScreen.css"
+
+export const StartScreen = () => {
+    return (
+        <div className="start">
+            <h1>Secret Word</h1>
+            <p>Clique no botão para começar o jogo</p>
+            <button>Começar o jogo</button>
+
+
+
+        </div>
+    )
+}
